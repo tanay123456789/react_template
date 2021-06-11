bcec66a269d837f07912256efc41e83bec4b1536
Tanay <tanaydwivedi456@gmail.com>
2021-06-11 10:19:19 +0530

    Template completed

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import NavBar from "./Navbar"
+
+import HeroSection from "./HeroSection"
+
+import CardSection from "./CardSection"
+
+const App= () =>{
+    return(
+        <div>
+            <h1>Tanay</h1>
+            <NavBar/>
+            <HeroSection/>
+            <CardSection/>
+        </div>
+    )
+}
+export default App;
